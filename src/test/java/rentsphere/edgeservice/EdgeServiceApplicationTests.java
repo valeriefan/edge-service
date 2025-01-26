9b3e444ed04f62757ebe3270fbd7aae90044bf2f
Valerie Fan <valeriefan1009@proton.me>
2025-01-26 22:16:25 +0800

    fix CSRF token

--- src/test/java/rentsphere/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/rentsphere/edgeservice/EdgeServiceApplicationTests.java
@@ -2,32 +2,32 @@ package rentsphere.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 class EdgeServiceApplicationTests {
 
     private static final int REDIS_PORT = 6379;
 
     @Container
-    static GenericContainer<?> redis =
-            new GenericContainer<>(DockerImageName.parse("redis:7.0"))
-                    .withExposedPorts(REDIS_PORT);
+    static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis"))
+            .withExposedPorts(REDIS_PORT);
+
+    @MockitoBean
+    ReactiveClientRegistrationRepository clientRegistrationRepository;
 
     @DynamicPropertySource
     static void redisProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.redis.host",
-                () -> redis.getHost());
-        registry.add("spring.redis.port",
-                () -> redis.getMappedPort(REDIS_PORT));
+        registry.add("spring.redis.host", () -> redis.getHost());
+        registry.add("spring.redis.port", () -> redis.getMappedPort(REDIS_PORT));
     }
 
     @Test
